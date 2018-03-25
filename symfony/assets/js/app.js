58c8eeaa776b54160748487c4231c6b7b2602de9
Nelson Moller <nmoller.c@gmail.com>
2018-03-25 16:38:54 -0400

    Testing frontend dev.

--- symfony/assets/js/app.js
+++ symfony/assets/js/app.js
@@ -2,3 +2,13 @@
 
 
 // ...rest of JavaScript code here
+//  loads the jquery package from node_modules
+var $ = require('jquery');
+
+// import the function from greet.js (the .js extension is optional)
+// ./ (or ../) means to look for a local file
+// var greet = require('./greet');
+
+$(document).ready(function() {
+    $('#container').append('<h1> john </h1>');
+});
